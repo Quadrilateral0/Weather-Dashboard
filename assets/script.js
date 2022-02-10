a9ca65509ffadbd371e1d228f92563433385e7db
Quadrilateral <amandacdulaney@gmail.com>
2022-02-09 23:01:04 -0500

    added second api fetch with lon & lat, fixed html error

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,14 @@ var text = document.getElementById("search-bar");
 var searchButton = document.getElementById("search-button");
 var searchDisplay = document.getElementById("search-append");
 var dataDisplay = document.querySelector("#data-display");
-var forecast = document.querySelector("#forecast");
+var forecastDisplay = document.querySelector("#forecast-display");
 var searchLocation = document.createElement("h3");
 var windLocation = document.createElement("p");
 var tempLocation = document.createElement("p");
 var humLocation = document.createElement("p");
 var feelsLocation = document.createElement("p");
+var lat = "";
+var lon = "";
 var searchItem = [];
 
 //Renders today's weather data according to search query
@@ -34,6 +36,11 @@ function getApi() {
       humLocation.textContent = "Current humidity: " + data.main.humidity + "%";
       feelsLocation.textContent = "Current temperature feels like: " + data.main.feels_like + "°F";
 
+      lat = data.coord.lat;
+        saveLat();
+      lon = data.coord.lon;
+        saveLon();
+
       dataDisplay.append(searchLocation);
       dataDisplay.append(tempLocation);
       dataDisplay.append(feelsLocation);
@@ -42,9 +49,47 @@ function getApi() {
     })
 };
 
+function getForecastApi() {
+  var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + APIKey;
+  //var outcome = {};
+  fetch(queryURL2)
+    .then(function (response) {
+      if (200 !== response.status) {
+        forecastDisplay.append(
+          "There was a problem with your query. Status Code: " + response.status
+        );
+        return;
+      }
+      return response.json();
+    })
+
+    //.then(function (data) {
+    //  console.log(data);
+    //  outcome.forecastResults = [];
+    //  for (i = 0; i < 5; i++) {
+    //    var forecast = {};
+    //    forecast.date = data[i].dt_txt;
+    //    forecast.minTemp = data[i].main.temp_min;
+    //    forecast.maxTemp = data[i].main.temp_max;
+    //    forecast.img = data[i].weather[0].icon;
+    //    forecast.desc = data[i].weather[0].description;
+    //    outcome.forecastResults.push(forecast);
+    //  }
+    //  console.log(outcome.forecastResults);
+    //});
+};
+
 //Store search queries in the searchItem array in local storage
 function saveSearch() {
   localStorage.setItem("searchItem", JSON.stringify(searchItem));
+};
+
+//Store latitude & longitude in local storage
+function saveLat() {
+  localStorage.setItem("lat", JSON.stringify(lat));
+};
+function saveLon() {
+  localStorage.setItem("lon", JSON.stringify(lon));
 };
 
 //Append search query to page
@@ -81,6 +126,7 @@ searchButton.addEventListener("click", function (event) {
   saveSearch();
   renderSearch();
   getApi();
+  getForecastApi();
 
   text.value = "";
 });
